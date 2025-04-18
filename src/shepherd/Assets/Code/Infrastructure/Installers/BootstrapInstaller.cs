a4f64a4cda4868d9065135f6d60c5e0c4bd2d6a3
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2025-04-18 18:15:15 -0400

    SceneLoading created.

--- src/shepherd/Assets/Code/Infrastructure/Installers/BootstrapInstaller.cs
+++ src/shepherd/Assets/Code/Infrastructure/Installers/BootstrapInstaller.cs
@@ -1,10 +1,11 @@
-﻿using Code.Infrastructure.States;
+﻿using Code.Infrastructure.Scenes;
+using Code.Infrastructure.States;
 using Code.Infrastructure.States.Factory;
 using Zenject;
 
 namespace Code.Infrastructure.Installers
 {
-  public class BootstrapInstaller : MonoInstaller, IInitializable
+  public class BootstrapInstaller : MonoInstaller, IInitializable, ICoroutineRunner
   {
     public void Initialize()
     {
@@ -13,6 +14,18 @@ namespace Code.Infrastructure.Installers
 
     public override void InstallBindings()
     {
+      BindInstaller();
+      BindInfrastructureServices();
+    }
+
+    private void BindInstaller()
+    {
+      Container.BindInterfacesTo<BootstrapInstaller>().FromInstance(this).AsSingle();
+    }
+
+    private void BindInfrastructureServices()
+    {
+      Container.Bind<ISceneLoader>().To<SceneLoader>().AsSingle();
       Container.Bind<IStateFactory>().To<StateFactory>().AsSingle();
       Container.Bind<IGameStateMachine>().To<GameStateMachine>().AsSingle();
     }
---
+++ src/shepherd/Assets/Code/Infrastructure/Scenes/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Code.Infrastructure.Scenes
+{
+  public class SceneLoader : ISceneLoader
+  {
+    private readonly ICoroutineRunner _coroutineRunner;
+
+    public SceneLoader(ICoroutineRunner coroutineRunner)
+    {
+      _coroutineRunner = coroutineRunner;
+    }
+
+    public void LoadScene(string sceneName, Action onLoaded = null)
+    {
+      _coroutineRunner.StartCoroutine(Load(sceneName, onLoaded));
+    }
+
+    private IEnumerator Load(string nextScene, Action onLoaded = null)
+    {
+      if (SceneManager.GetActiveScene().name != nextScene)
+      {
+        onLoaded?.Invoke();
+        yield break;
+      }
+      
+      AsyncOperation waitNextScene = SceneManager.LoadSceneAsync(nextScene);
+      
+      while(!waitNextScene.isDone)
+        yield return null;
+      
+      onLoaded?.Invoke();
+    }
+  }
+}
